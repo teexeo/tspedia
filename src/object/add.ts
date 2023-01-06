34e7a0ee16e5e88921adbb636ff81acc01dec32d
Suleyman <suleyman91002@gmail.com>
2023-01-06 14:32:44 +0500

    add and divide function docs changed

--- src/object/add.ts
+++ src/object/add.ts
@@ -1,8 +1,10 @@
 /**
  * ```ts
- * $.add({ name: "Adam" }, "age", 20) // { name: "Adam", age: 20 }
+ * $.add({ name: "Adam" }, "age", 20) 
+ * // => { name: "Adam", age: 20 }
  *  
- * $.add({ name: "Adam", age: 20 }, "age", 22) // { name: "Adam", age: 22 } 
+ * $.add({ name: "Adam", age: 20 }, "age", 22) 
+ * // => { name: "Adam", age: 22 } 
  * ```
  * @param obj must be a object
  * @param key string | number
--- src/object/divide.ts
+++ src/object/divide.ts
@@ -1,3 +1,23 @@
+/**
+ * ```ts
+ *console.log(
+    $.divide({
+        name: "Suleyman",
+        age: 20,
+        jobs: ["Programming", "Student"],
+    })
+  );
+  // {
+  //    keys: [ 'name', 'age', 'jobs' ],
+  //    values: [ 'Suleyman', 20, [ 'Programming', 'Student' ] ]
+  // }
+  ```
+
+ *  
+ * @param obj object
+ * @returns 
+ */
+
 export function divide (obj: object) {
     if (typeof obj !== "object" || Array.isArray(obj)) return false;
     return {
